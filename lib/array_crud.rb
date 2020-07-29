90874af036672eb948a294036af397d2a5ea8c73
Haley Proctor <hproctor021@gmail.com>
2020-07-29 19:52:46 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,40 @@
+$carrots = ["orange", "yellow", "am", "purple", "rainbow"]
+
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  carrots = ["orange", "yellow", "purple", "rainbow"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(carrots, big)
+  array = $carrots
+  element = "arrays!"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  array = $carrots
+  element = "wow"
+  array.unshift(element)
 end
 
 def remove_element_from_end_of_array(array)
+  array = $carrots
+  array.pop
   
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = $carrots
+  array.shift()
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = $carrots
+  #index_number == 2
+  array.index(2)
 end
 
 def retrieve_first_element_from_array(array)
