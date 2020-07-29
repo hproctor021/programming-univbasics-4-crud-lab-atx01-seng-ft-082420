1a539d6472167f8edb767430e723a6493523db23
Haley Proctor <hproctor021@gmail.com>
2020-07-29 19:55:48 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,7 +43,8 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = $carrots
+  array[6]
 end
 
 def update_element_from_index(array, index_number, element)
